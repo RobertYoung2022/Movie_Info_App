fc082ebe52101b134469285eeed7aeede17930ea
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-02-12 14:47:57 -0600

    search function can be seen via console log -- bootwatch not functional

---
+++ movie-app.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+    $('#searchForm').on('submit', (e) => {
+        let searchText = $('#searchText').val();
+        getMovies(searchText);
+        e.preventDefault();
+    });
+});
+
+function getMovies(searchText) {
+    console.log(searchText);
+}
