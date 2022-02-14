42fa6962704649d11f6f228481cec63ceddafc4e
Robert Young <94646951+RobertYoung2022@users.noreply.github.com>
2022-02-14 09:07:19 -0600

    changed project instruction

---
+++ config.js
@@ -0,0 +1,5 @@
+export const config = {
+    api_key: '39b4edd74e5d99573e345298b11805a8',
+    api_base_url: 'https://api.themoviedb.org/3/movie/550?api_key=',
+    image_base_url: 'https://image.tmdb.org/t/p/w1280'
+}
--- movie-app.js
+++ movie-app.js
@@ -1,3 +1,44 @@
+import { config } from "./config.js"
+
+const BASE_URL = config.api_base_url
+const API_KEY = config.api_key
+
+export async function getPopularMovies(page = 1) {
+    let data = []
+    try {
+        const response = await fetch(`${BASE_URL}movie/popular?api_key=${API_KEY}&page=${page}`)
+        const responseData = await response.json()
+        data = responseData?.results
+    } catch (error) {
+
+    }
+    return data
+}
+
+const moviesDiv = document.getElementById("movies")
+
+import { getPopularMovies } from "./api.js";
+import { config } from "./config.js";
+
+export async function renderMovies() {
+    const movies = await getPopularMovies()
+    console.log(movies)
+    moviesDiv.innerHTML = movies?.map(movie => renderSingleMovie(movie)).join("")
+}
+
+function renderSingleMovie(movie) {
+    return (
+        `
+        <div class="col-4 col-lg-3 col-xl-2 p-1">
+            <img src="${config.image_base_url + movie?.poster_path}" class="img-fluid" >
+        </div>
+        `
+    )
+}
+
+
+
+/*
 $(document).ready(() => {
     $('#searchForm').on('submit', (e) => {
         let searchText = $('#searchText').val();
@@ -6,6 +47,13 @@ $(document).ready(() => {
     });
 });
 
-function getMovies(searchText) {
-    console.log(searchText);
-}
+function getMovies(searchText, axios1 = axios) {
+    axios1.get('https://www.omdbapi.com?s=' + searchText)
+        .then((response) => {
+            console.log(response);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}*/
+
